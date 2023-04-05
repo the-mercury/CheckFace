23b459617c79d8e4c4aedd2cdfb89e89c981d338
mercury <themercuryking@gmail.com>
2023-04-05 09:56:57 -0400

    [enhancement] add camera model update view to camera controller

--- controller/camera_controller.py
+++ controller/camera_controller.py
@@ -1,4 +1,6 @@
-from PyQt6.QtCore import QObject, QMutex, QWaitCondition
+import numpy as np
+from PyQt6.QtCore import pyqtSlot, QObject, QMutex, QWaitCondition
+from PyQt6.QtGui import QPixmap, QImage
 
 from controller.overlay_controller import OverlayController
 
@@ -26,6 +28,22 @@ class CameraController(QObject):
         self.camera_view.reset_button.clicked.connect(self.__on_reset_button_pressed)
         self.camera_view.continue_button.clicked.connect(self.__on_continue_button_pressed)
         self.camera_view.try_again_button.clicked.connect(self.__on_try_again_button_pressed)
+
+    @pyqtSlot(np.ndarray)
+    def __update_view(self, frame):
+        CameraController.mutex.lock()
+        height, width = frame.shape[:2]
+        convert_to_qt_format = QImage(frame.data.tobytes(), width, height, QImage.Format.Format_RGB888)
+        image_pix_map = QPixmap.fromImage(convert_to_qt_format)
+        self.camera_view.set_pix_map(image_pix_map)
+        self.overlay_controller.detect_face_status(frame)
+        CameraController.mutex.unlock()
+        CameraController.wait_condition.wakeAll()
+
+    @pyqtSlot(int)
+    def __update_view_size(self, square_frame_size):
+        self.camera_view.square_frame(square_frame_size)
+        self.overlay_controller.update_overlay_mask(square_frame_size)
 
     def start(self):
         CameraController.mutex.lock()
